726a1326dc013008cdc9cbe9f0601e11b43a08f9
Terry Harmon <tharmon0717@gmail.com>
2025-09-23 20:15:00 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
 ask_should_symlink() {
+  local src="$1"
+  local dest="$2"
+
   while true; do
-    read -p "Do you want to symlink $1 to $2 ? " yn
+    read -p "Do you want to symlink $src -> $dest ? [y/n] " yn
     case $yn in
-    [Yy]*)
-      symlink_safe $1 $2
-      break
-      ;;
-    [Nn]*) return ;;
-    *) echo "Please answer yes or no." ;;
+      [Yy]*)
+        ln -sfn "$src" "$dest"
+        break
+        ;;
+      [Nn]*) return ;;
+      *) echo "Please answer yes or no." ;;
     esac
   done
 }
 
 symlink_or_ask() {
-  if [ -f $2 ]; then
-    ask_should_symlink $1 $2
+  local src="$1"
+  local dest="$2"
+
+  if [ -e "$dest" ] || [ -L "$dest" ]; then
+    ask_should_symlink "$src" "$dest"
   else
-    ln -s $1 $2
+    ln -s "$src" "$dest"
   fi
 }
 
-# Symlinks
-symlink_or_ask ~/.dotfiles/nvim ~/.config/nvim
-symlink_or_ask ~/.dotfiles/aerospace ~/.config/aerospace
-symlink_or_ask ~/.dotfiles/ghostty ~/.config/ghostty
-symlink_or_ask ~/.dotfiles/k9s ~/.config/k9s
-symlink_or_ask ~/.dotfiles/.antigenrc ~/.antigenrc
-symlink_or_ask ~/.dotfiles/.zshrc ~/.zshrc
+DOTFILES="$HOME/.dotfiles"
+
+# Nuke neovim config
+rm -rf ~/.config/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.local/share/nvim
+
+# Install
+symlink_or_ask "$DOTFILES/nvim" "$HOME/.config/nvim"
+symlink_or_ask "$DOTFILES/aerospace" "$HOME/.config/aerospace"
+symlink_or_ask "$DOTFILES/ghostty" "$HOME/.config/ghostty"
+symlink_or_ask "$DOTFILES/k9s" "$HOME/.config/k9s"
+symlink_or_ask "$DOTFILES/.antigenrc" "$HOME/.antigenrc"
+symlink_or_ask "$DOTFILES/.zshrc" "$HOME/.zshrc"
